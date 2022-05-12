1e3f37e5c24477ae8fd93670ff60790dfa5f6f66
Graef <simonjn45@gmail.com>
2022-05-12 19:22:25 +0100

    Now Compile with Clang

--- build.sh
+++ build.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
 
-# I am using the Tiny C Compiler here
-# As tcc | could also use Clang 
+# I am using te Clang Compiler here
+# Should work with tcc and clang 
 
 # To make the object files
 echo "> Making the object files"
 # The compiled files : compiler -c nameoffile -Wall
-tcc -c chunk.c -Wall
-tcc -c compiler.c -Wall
-tcc -c debug.c -Wall
-tcc -c memory.c -Wall
-tcc -c object.c -Wall
-tcc -c scanner.c -Wall
-tcc -c table.c -Wall
-tcc -c value.c -Wall
-tcc -c vm.c -Wall
+clang -c chunk.c -Wall
+clang -c compiler.c -Wall
+clang -c debug.c -Wall
+clang -c memory.c -Wall
+clang -c object.c -Wall
+clang -c scanner.c -Wall
+clang -c table.c -Wall
+clang -c value.c -Wall
+clang -c vm.c -Wall
 echo "> Finished making the object files"
 
 # To make the executable
 echo "> Making the executable"
 # The main file compiled like : compiler -o executablename name objectfiles -Wall
-tcc -g -o clox.exe main.c chunk.o compiler.o debug.o memory.o object.o table.o scanner.o value.o vm.o -Wall
+clang -g -o clox.exe main.c chunk.o compiler.o debug.o memory.o object.o table.o scanner.o value.o vm.o -Wall
 echo "> Finished making the exectable"
 
 # delete the object files
 echo "> Deleting the object files"
+# rm the clang generated files when clang is used
+# if not comment the following files out
+rm clox.ilk
+rm clox.pdb
 # rm the made object files ( and more if you are using clang for example )
 rm chunk.o
 rm compiler.o
